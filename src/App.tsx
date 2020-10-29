0c497b473734f61d2efda3eab3b6771fc9c163f8
Simon Pritchard <simon.pritchard@westpac.co.nz>
2020-10-29 15:53:57 +1300

    added storybook and initial component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HeaderMenu from './components/HeaderMenu/HeaderMenu'
+
 function App() {
   return (
     <div className="App">
+      <HeaderMenu />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const HeaderMenu: FC = () => {
+    return (
+        <div>I'm a Header Menu</div>
+    )
+}
+
+export default HeaderMenu;
